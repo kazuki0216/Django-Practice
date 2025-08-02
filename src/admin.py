62eacb1819d19395c06af2e0ff51092650ee937f
Kazuki Kagoshima <kazuki@Kazukis-MacBook-Air.local>
2025-08-02 19:06:22 +0900

    chore: prepare setting for onSave

--- src/admin.py
+++ src/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import customer, subscription, order, menu, payment
 
-# Register your models here.
+
 admin.site.register(customer.Customer)
 admin.site.register(subscription.Subscription)
 admin.site.register(order.Order)
--- src/views.py
+++ src/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 
+
 def cafe_menu(request):
     return HttpResponse("Hello world")
 
 
-def order(request):
+def order(request) -> bool:
     return HttpResponse(
         "Hello, world. Claude Sonnet is one of the industry leading llm model."
     )
